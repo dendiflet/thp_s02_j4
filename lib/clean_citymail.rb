a75c621b695fac653db89178015a3091555da28d
dendiflet <bretin.iannis@gmail.com>
2019-01-17 23:22:40 +0100

    mairie scrap fini beau pas testé

--- lib/clean_citymail.rb
+++ lib/clean_citymail.rb
@@ -16,43 +16,126 @@ require 'nokogiri'
 require 'open-uri'
 
 
+
+
+
 def all_dep
-	pays_doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/"))
+
+	scrappagetotal = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/"))
+	
+	#selectionne les liens du corp de page avec la class "lientxt"
+	dep_links_dirty = scrappagetotal.css("tbody").css("a").select{|link| link['class'] == "lientxt"}
+
+	mails_each_dep = []
+	
+	#création de l'array de mails + mise en forme des mails
+	dep_links_dirty.each{|link| mails_each_dep << "https://www.annuaire-des-mairies.com/#{link['href'].to_s}"}
+	#puts mails_each_dep
+
+	return mails_each_dep
+end
+
+def true_city_mail_list(dep_base_list)
+
+	dep_base_list.each do |mail|								#loop avec chaque mail de la liste
+
+		if check_page(mail) == true 							#verif du mail donné <-tjrs true donc set page exist = true
+			dep_pages_full_list << mail 									#ajout a la full_list
+		end
+
+		i = 2																			#les pages supp commencent an -2.html
+		temp_dep_mail = ""
+
+		until page_exist == false do		
+
+			temp_dep_mail = dep_base_list.insert(-6;"-#{i}")		#modificaton du mail d'origine
+			
+			check_page(temp_dep_mail)									#fait tourner pour voir si la page existe
+			
+			if page_exist == true 									#si la page existe ajout a la liste
+				dep_pages_full_list << temp_dep_mail
+			i += 1 																	#iteration pour la prochaine verif
+			end
+
+		end
+	end
+
+	return dep_pages_full_list
+end
+
+
+def check_page(url)
+	begin
+    open(url)
+  rescue OpenURI::HTTPError
+    page_exist = false
+    return
+  end
+  	page_exist = true
+  return
 end
 
 
 
 
-def all_commune
+def collect_all_citys_page()
 
 	scrappagetotal = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/vosges.html"))
 	
 	#selectionne les liens du corp de page avec la class "lientxt"
 	news_links_dirty = scrappagetotal.css("tr").css("a").select{|link| link['class'] == "lientxt"}
 
-	mails_each_city = []
+	mails_each_city_dep_page_list = []
 	
 	#création de l'array de mails + mise en forme des mails
-	news_links_dirty.each{|link| mails_each_city << "https://www.annuaire-des-mairies.com/#{link['href'].to_s.slice!(1..-1)}"}
+	news_links_dirty.each{|link| mails_each_city_dep_page_list << "https://www.annuaire-des-mairies.com/#{link['href'].to_s.slice!(1..-1)}"}
 
-	#puts mails_each_city
-
+	#puts mails_each_city_dep_page_list
+	return mails_each_city_dep_page_list
 end
 
 
-def mail_citytwnhll
+def collect_all_citys_mail
 
 	city_doc_all = Nokogiri::HTML(open("http://annuaire-des-mairies.com/88/epinal.html"))
-  
-  cty_twnhall = city_doc_all.xpath("//tbody/tr[4]/td[2]")
+  #va chercher a peux pres au bon endroit
+  cty_twnhall = city_doc_all.xpath("//body/div/main/section[2]/div/table/tbody/tr[4]/td[2]") 		#ultra presci...
+  #puts cty_twnhall
+
+
   city_mail = []
-  #transfo en string (car c'est encore un node), selectionne la bonne et clean car encore les <td>
-  city_mail = cty_twnhall[0].to_s.slice(4...-5)										#.select { |v| v =~ /[@]/ } 
+  #transfo en string (car c'est encore un node), ###selectionne la bonne####fini#### et clean car encore les <td>
+  city_mail = cty_twnhall.to_s.slice(4...-5)										#### ceci ne fonctionne pas -->  .select { |v| v =~ /[@]/ } 
+  
+  #puts city_mail
+  return city_mail
+end
 
-  puts city_mail
+def perform
+	city_list_page = all_dep			#recherche des mails de chaque departements
+	
+	dep_pages_full_list = true_city_mail_list(city_list_page)		#il y avait plusieurs pages de communes ;-)
+	
+	dep_pages_full_list.each do |dep_page|															#collecte des liens de chaque mairies avec une boucle pour chaque
+		full_city_page_list << collect_all_citys_page(dep_page)
+	end
 
+
+		full_city_page_list.each do |city_page|															#collecte des liens de chaque mairies avec une boucle pour chaque
+		full_city_mails_list << collect_all_citys_mail(city_page)
+	end
+	return full_city_mails_list
 
 end
 
+full_list = perform
 
-#mail_citytwnhll
+print full_list
+
+
+
+
+
+
+
+
--- lib/scrapping_emails_cityhall.rb
+++ lib/scrapping_emails_cityhall.rb
@@ -15,38 +15,74 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
+#/html/body/div/main/section[2]/div/table/tbody/tr[1]/td[1]/a
+
 
 def all_dep
-	pays_doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/"))
+	scrappagetotal = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/"))
+	
+	#selectionne les liens du corp de page avec la class "lientxt"
+	dep_links_dirty = scrappagetotal.css("tbody").css("a").select{|link| link['class'] == "lientxt"}
+
+	mails_each_dep = []
+	
+	#création de l'array de mails + mise en forme des mails
+	dep_links_dirty.each{|link| mails_each_dep << "https://www.annuaire-des-mairies.com/#{link['href'].to_s}"}
+	#puts mails_each_dep
+
+	return mails_each_dep
+end
+
+
+#lien = "https://www.annuaire-des-mairies.com/isere.html"
+
+
+def true_city_mail_list
+
+	mails_each_dep.each do |mail|
+
+		if check_page(mail) == true
+			true_dep_mail_list << mail
+		end
+
+		i = 2
+		temp_dep_mail = ""
+
+		until page_exist == false do
+			
+			temp_dep_mail = mail.insert(-6;"-#{i}")		#modificaton du mail d'origine
+			
+			check_page(temp_dep_mail)									#fait tourner pour voir si la page existe
+			
+			if page_exist == true
+				true_dep_mail_list << temp_dep_mail
+			i += 1
+			end
+		end
+	end
+
+	return true_dep_mail_list
+end
+
+
+def check_page(url)
+	begin
+    open(url)
+  rescue OpenURI::HTTPError
+    page_exist = false
+    return
+  end
+  	page_exist = true
+  return
 end
 
 
 
-#def all_commune
-
-page = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/vosges.html"))
-#departement_doc.each{|link| puts link['href'] }
-
-news_links = page.css("tr").css("a").select{|link| link['class'] == "lientxt"}
-#### ca reste un node avec trop de chose mais selectionne que les bons #######
-###########news_links.each{|link| puts " babrare  #{link['href']}" }				# ajout fonctionne
-#news_links.each {|link| puts link['href.slice(1..-1)']  }										#non, disparait
-
-#news_links.each{|link| puts "https://www.annuaire-des-mairies.com/#{link['href'].to_s.slice!(1..-1)}"}					######GGGGGOOOOOODDDDD
-mails_each_city = []
-news_links.each{|link| mails_each_city <<  "https://www.annuaire-des-mairies.com/#{link['href'].to_s.slice!(1..-1)}"}
-
-print mails_each_city
 
 
 
-# news_links.each{|link| puts " babrare  #{link['href']}" }
 
-# news_links.each{|link| puts " babrare  #{link['href']}" }
-
-# news_links.each{|link| puts " babrare  #{link['href']}" }
-
-# news_links.each{|link| puts " babrare  #{link['href']}" }
+puts check_page("https://www.annuaire-des-mairies.com/cantal-2.html")
 
 
 
@@ -54,26 +90,63 @@ print mails_each_city
 
 
 
-#liens =  departement_doc.css("a.lientxt")['href']
-#puts liens																	#j'ai toute <a.....>
-#puts "#{liens.map do |i| i=i["href"] end}"
-#	puts liens
-
-
-#.css("a[@class=lientxt]/@href").attribute("href")
-
-# commune_list0 = 
-#puts commune_list0
 
 
 
 
+# #def all_commune
 
-def mail_citytwnhll
+# page = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/vosges.html"))
+# #departement_doc.each{|link| puts link['href'] }
 
-city_doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/88/epinal.html"))
-    cty_twnhall = city_doc.xpath("//tbody/tr[4]/td[2]")
-    print cty_twnhall
-    city_mail = cty_twnhall.select { |v| v =~ /[@]/ } 
+# news_links = page.css("tr").css("a").select{|link| link['class'] == "lientxt"}
+# #### ca reste un node avec trop de chose mais selectionne que les bons #######
+# ###########news_links.each{|link| puts " babrare  #{link['href']}" }				# ajout fonctionne
+# #news_links.each {|link| puts link['href.slice(1..-1)']  }										#non, disparait
 
-end
+# #news_links.each{|link| puts "https://www.annuaire-des-mairies.com/#{link['href'].to_s.slice!(1..-1)}"}					######GGGGGOOOOOODDDDD
+
+
+# mails_each_city = []
+# news_links.each{|link| mails_each_city << "https://www.annuaire-des-mairies.com/#{link['href'].to_s.slice!(1..-1)}"}
+# print mails_each_city
+
+
+
+# # news_links.each{|link| puts " babrare  #{link['href']}" }
+
+# # news_links.each{|link| puts " babrare  #{link['href']}" }
+
+# # news_links.each{|link| puts " babrare  #{link['href']}" }
+
+# # news_links.each{|link| puts " babrare  #{link['href']}" }
+
+
+
+
+
+
+
+# #liens =  departement_doc.css("a.lientxt")['href']
+# #puts liens																	#j'ai toute <a.....>
+# #puts "#{liens.map do |i| i=i["href"] end}"
+# #	puts liens
+
+
+# #.css("a[@class=lientxt]/@href").attribute("href")
+
+# # commune_list0 = 
+# #puts commune_list0
+
+
+
+
+
+# def mail_citytwnhll
+
+# city_doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/88/epinal.html"))
+#     cty_twnhall = city_doc.xpath("//tbody/tr[4]/td[2]")
+#     print cty_twnhall
+#     city_mail = cty_twnhall.select { |v| v =~ /[@]/ } 
+
+# end
